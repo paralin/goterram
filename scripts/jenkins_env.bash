909b1dcc55b8c7e5270b7dbdd56c64a099420288
Christian Stewart <christian@paral.in>
2016-09-20 13:32:07 -0400

    chore(release): release the release branch only

--- scripts/jenkins_env.bash
+++ scripts/jenkins_env.bash
@@ -3,7 +3,7 @@ set -e
 . ~/.bashrc
 export GOPATH=$(pwd)/goworkspace
 export PATH=$PATH:${GOPATH}/bin
-if ./is_master.bash; then
+if ./scripts/is_master.bash; then
   export JENKINS_SHOULD_RELEASE=ismaster
 else
   unset JENKINS_SHOULD_RELEASE
