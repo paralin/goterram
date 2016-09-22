bfeaa1435285d0d26aa54b5bedd6b253c511115a
Christian Stewart <christian@paral.in>
2016-09-22 00:19:27 -0400

    feat(frontend): implement frontend basic js wrappers

--- js/goterram.d.ts
+++ js/goterram.d.ts
@@ -1,7 +1,36 @@
 declare module "@fusebot/goterram" {
   let ITerramGame: any;
   declare interface ITerramJsGlobal {
-    BuildTerramGame(): ITerramGame;
+    BuildTerramGame(frontend: IFrontend): ITerramGame;
+  }
+
+  declare interface INetEntity {
+    id: Number;
+    parent_id?: Number;
+  }
+
+  declare interface IFrontendComponent {
+    init?(): void;
+    destroy?(): void;
+  }
+
+  declare interface IFrontendEntity {
+    // Called when entity is created, no components
+    init?(): void;
+    // Called when adding components
+    addComponent?(component_id: number): IFrontendComponent | void;
+    // Called when all components have been added.
+    initLate?(): void;
+    // Called when the entity is removed.
+    destroy?(): void;
+  }
+
+  /* Frontend interface */
+  declare interface IFrontend {
+    init?(): void;
+    // Called when an entity is added.
+    addEntity?(entity: INetEntity): IFrontendEntity | void;
+    destroy?(): void;
   }
 
   export = ITerramJsGlobal;
--- js/goterram_js.go
+++ js/goterram_js.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/fuserobotics/goterram/factory"
 	"github.com/gopherjs/gopherjs/js"
 )
@@ -9,12 +10,14 @@ import (
 type TerramJsGlobal struct {
 }
 
-func (*TerramJsGlobal) BuildTerramGame() *js.Object {
-	game, err := factory.BuildTerramGame()
+func (*TerramJsGlobal) BuildTerramGame(frontend *JsFrontend) *js.Object {
+	game, err := factory.BuildTerramGame(frontend)
+
 	if err != nil {
 		fmt.Printf("Error building game: %s\n", err.Error())
 		return nil
 	}
+
 	// return game
 	return js.MakeWrapper(game)
 }
